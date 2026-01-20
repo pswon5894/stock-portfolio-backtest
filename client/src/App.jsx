d54aac4e3e00c4e5e35ea77485078812b3e4151b
pswon5894 <pswon5894@gmail.com>
2026-01-20 12:59:21 +0900

    1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -38,7 +38,7 @@ function App() {
 
   // 서버 연결 테스트
   useEffect(() => {
-    fetch('https://pswon5894.github.io/stock-portfolio-backtest/')
+    fetch('https://stock-portfolio-backtest.onrender.com')
       .then(res => res.json())
       .then(data => {
         setServerStatus('✅ 연결됨');
