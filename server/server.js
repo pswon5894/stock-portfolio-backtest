5caea60d50bb2b7af507a3e173b2e15d4c5320ce
pswon5894 <pswon5894@gmail.com>
2026-01-28 20:45:55 +0900

    cors, vercel, server, render

--- server/server.js
+++ server/server.js
@@ -25,7 +25,8 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5000',
   // 'https://pswon5894.github.io/stock-portfolio-backtest/'
-  'https://pswon5894.github.io'
+  'https://pswon5894.github.io',
+  'https://stock-portfolio-backtest.vercel.app'
 ];
 
 const corsOptions = {
